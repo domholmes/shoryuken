fe73f783d4107753bdeffe7f1c7111df3652c85a
Dom H <thecapsaicinkid@gmail.com>
2013-02-16 12:43:32 +0000

    Separating out repository logic, tidying up viewmodel

--- spa/PodcastTest/Scripts/viewmodels/episodesViewModel.js
+++ spa/PodcastTest/Scripts/viewmodels/episodesViewModel.js
@@ -1,66 +1,11 @@
-
-/// <reference path="..\breeze.debug.js" />
-
-(function (root) {
-    var ko = root.ko,
-        breeze = root.breeze,
-        logger = root.app.logger;
-
-    // define the viewmodel
-    var vm = {
-        episodes: ko.observableArray(),
-        saveChanges: saveChanges,
-        refresh: getEpisodes
-        };
-
-    var episodeManager = new breeze.EntityManager('api/Episode');
-
-    // start fetching Todos
-    getEpisodes();
-
-    // re-query when "includeDone" checkbox changes
-    //vm.includeDone.subscribe(getEpisodes);
-
-    // bind view to the viewmodel
-    ko.applyBindings(vm);
-
-    function getEpisodes() {
-
-        var query = breeze.EntityQuery.from("Episodes");
-
-        logger.info("querying Episodes remotely");
-        
-        episodeManager
-            .executeQuery(query)
-            .then(querySucceeded)
-            .fail(queryFailed);
-
-        // reload vm.todos with the results 
-        function querySucceeded(data) {
-            logger.success("queried Episodes, writing to storage");
-            var exportData = episodeManager.exportEntities();
-            window.localStorage.setItem("cache", exportData);
-            vm.episodes(data.results);
-        }
-    };
-
-    function queryFailed(error) {
-        logger.error("Query failed: " + error.message);
+// define the viewmodel
+var vm = {
+    episodes: ko.observableArray(),
+    saveChanges: saveChanges,
+    refresh: function () {
+        return populateEpisodes(this.episodes);
     }
+};
 
-    function saveChanges() {
-        return episodeManager.saveChanges()
-            .then(function () { logger.success("changes saved"); })
-            .fail(saveFailed);
-    }
-
-    function saveFailed(error) {
-        logger.error("Save failed: " + error.message);
-    }
-
-    function clearLocalStorage() {
-        window.localStorage.clear();
-    }
-    //#endregion
-
-} (window));
+// bind view to the viewmodel
+ko.applyBindings(vm);
