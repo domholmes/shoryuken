91af2ef270ee1f499eecb1f459ef0a45cabddaea
Dom H <thecapsaicinkid@gmail.com>
2013-02-16 12:43:32 +0000

    Separating out repository logic, tidying up viewmodel and fixing page not refreshing

---
+++ spa/PodcastTest/Scripts/app/episodeRepository.js
@@ -0,0 +1,46 @@
+(function (window) {
+
+    var episodeManager = new breeze.EntityManager('api/Episode');
+
+    function populateEpisodes(viewModel) {
+
+        var query = breeze.EntityQuery.from("Episodes");
+
+        app.logger.info("querying Episodes remotely");
+
+        episodeManager
+        .executeQuery(query)
+        .then(querySucceeded)
+        .fail(queryFailed);
+
+        function querySucceeded(data) {
+            app.logger.success("queried Episodes");
+            var exportData = episodeManager.exportEntities();
+            window.localStorage.setItem("cache", exportData);
+            app.logger.success("written to storage");
+            viewModel.episodes(data.results);
+            app.logger.success("viewmodel updated");
+        }
+
+        function queryFailed(error) {
+            app.logger.error("Query failed: " + error.message);
+        }
+    };
+
+    function saveChanges() {
+        return episodeManager.saveChanges()
+        .then(function () { app.logger.success("changes saved"); })
+        .fail(saveFailed);
+    }
+
+    function saveFailed(error) {
+        app.logger.error("Save failed: " + error.message);
+    }
+
+    // expose a repository interface to the window object
+    window.repository = {
+        populateEpisodes: populateEpisodes,
+        saveChanges: saveChanges
+    }
+
+} (window))
