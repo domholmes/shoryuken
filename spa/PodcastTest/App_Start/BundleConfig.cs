ec6d32d5303d800acfa22f0c50f4b2a210607c45
Dom H <thecapsaicinkid@gmail.com>
2013-02-17 18:51:37 +0000

    Fixing .js bundles

--- spa/PodcastTest/App_Start/BundleConfig.cs
+++ spa/PodcastTest/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace PodcastTest
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = true;
+            
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -21,7 +23,7 @@ namespace PodcastTest
             bundles.Add(new ScriptBundle("~/bundles/mvvm").Include(
                         "~/Scripts/q.js", 
                         "~/Scripts/knockout-{version}.js", 
-                        "~/Scripts/breeze.debug.js"));
+                        "~/Scripts/breeze.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
