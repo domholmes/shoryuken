718d54deaf4762f3ff88fd4210cf465c9fcb5d9f
Dale <dshoulders@outlook.com>
2013-04-24 23:23:40 +0100

    Update JS packages
    Create view model
    Create Reminder constructor

---
+++ spa/SmartReminder/Scripts/sr.js
@@ -0,0 +1,46 @@
+﻿var sr = sr || {}; // our app namespace
+
+sr.Reminder = function (options) {
+    var r = this,
+        today = new Date(),
+        defaults;
+
+    // set up default options 
+    defaults = {
+        id: null,
+        name: '',
+        message: '',
+        details: '',
+        startTime: new Date(today.setHours(8, 0, 0, 0)),
+        endTime: new Date(today.setHours(20, 0, 0, 0)),
+        date: today,
+        days: [],
+        repeat: false,
+        active: true
+    };
+
+    $.extend(r, defaults, options);
+
+    // convert properties to observables
+    for (var prop in r) {
+        if (r.hasOwnProperty(prop)) {
+            if (r[prop] instanceof Array) {
+                r[prop] = ko.observableArray(r[prop]);
+            } else {
+                r[prop] = ko.observable(r[prop]);
+            }
+        }
+    }
+}
+
+sr.ViewModel = function () {
+    var vm = this;
+
+    vm.reminders = ko.observableArray([]);
+};
+
+ko.applyBindings(new sr.ViewModel());
+
+// temp test
+var test = new sr.Reminder()
+console.log(test);
