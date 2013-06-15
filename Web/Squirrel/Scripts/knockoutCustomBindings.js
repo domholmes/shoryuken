078428587841f2ca81df3433a965ebfb808cf7ea
Dale <dshoulders@outlook.com>
2013-06-15 19:25:38 +0100

    create new opacity bug

---
+++ Web/Squirrel/Scripts/knockoutCustomBindings.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    var customBindings = {
+
+        invisible: {
+            init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+                $(element).addClass('invisible-transition');
+            },
+            update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+                var value = valueAccessor()
+                if (value === true) {
+                    $(element).addClass('invisible');
+                } else {
+                    $(element).removeClass('invisible');
+                }
+            }
+        }
+
+    }
+
+    $.extend(ko.bindingHandlers, customBindings);
+})();
