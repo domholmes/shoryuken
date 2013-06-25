c3901449a8844b9183be83944f8d8d0b707de091
Dom H <thecapsaicinkid@gmail.com>
2013-06-25 21:39:38 +0100

    Auto migrations

--- Web/Squirrel/Migrations/Configuration.cs
+++ Web/Squirrel/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace Squirrel.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(Squirrel.Models.ReminderContext context)
