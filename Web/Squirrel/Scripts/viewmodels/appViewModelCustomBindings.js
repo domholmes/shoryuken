c00132d59f7f4906bd10d93969167a535682f6e7
Dom H <thecapsaicinkid@gmail.com>
2013-07-09 23:27:23 +0100

    Refactoring view logic out of viewmodel

---
+++ Web/Squirrel/Scripts/viewmodels/appViewModelCustomBindings.js
@@ -0,0 +1,16 @@
+﻿ko.bindingHandlers.slideVisible = {
+    update: function (element, valueAccessor, allBindingsAccessor) {
+
+        // Next, whether or not the supplied model property is observable, get its current value
+        ko.utils.unwrapObservable(valueAccessor());
+
+        // Grab some more data from another binding property
+        var duration = allBindingsAccessor().slideDuration || 400; // 400ms is default duration unless otherwise specified
+
+        // Now manipulate the DOM element
+        if (valueAccessor() == true)
+            $(element).slideDown(duration); // Make the element visible
+        else
+            $(element).hide();   // Make the element invisible
+    }
+};
