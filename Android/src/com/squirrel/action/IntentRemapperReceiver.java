103767baff94b4736dfdf5da2a5bfd66960d63e5
Dom H <thecapsaicinkid@gmail.com>
2013-08-15 20:57:57 +0100

    Wifi disconnect not being detected fix
    Changed IntentRemapperReceiver to listen for a different action which should be more reliable in detecting disconnect events on different devices. Fixes #21

--- Android/src/com/squirrel/action/IntentRemapperReceiver.java
+++ Android/src/com/squirrel/action/IntentRemapperReceiver.java
@@ -3,6 +3,7 @@ package com.squirrel.action;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
 import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiManager;
 
@@ -16,12 +17,13 @@ public class IntentRemapperReceiver extends BroadcastReceiver
 	{
 		String action = intent.getAction();
 
-		if (action.equals(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION))
+		if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION))
 		{
             WifiManager manager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
-            SupplicantState state = intent.getParcelableExtra(WifiManager.EXTRA_NEW_STATE);
+            NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+            NetworkInfo.State state = networkInfo.getState();
 
-			if(state == SupplicantState.COMPLETED)
+			if(state == NetworkInfo.State.CONNECTED)
 			{
                 String connectingToSsid = manager.getConnectionInfo().getSSID().replace("\"", "");
                 WifiStateHistory.recordConnectedSsid(connectingToSsid);
@@ -33,8 +35,8 @@ public class IntentRemapperReceiver extends BroadcastReceiver
                 }
 			}
 
-			if(state == SupplicantState.DISCONNECTED || state == SupplicantState.SCANNING)
-			{
+            if(state == NetworkInfo.State.DISCONNECTED)
+            {
 				if(manager.isWifiEnabled())
 				{
                     String disconnectedFromSsid = WifiStateHistory.getLastConnectedSsid();
