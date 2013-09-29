b76614b94ea5b8f8a1c12046fc40cc7404d775d7
Dale <dshoulders@outlook.com>
2013-09-29 18:45:58 +0100

    Added bundle for index page scripts

--- Web/Squirrel/App_Start/BundleConfig.cs
+++ Web/Squirrel/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace Squirrel
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = true;
+
             bundles.Add(new ScriptBundle("~/bundles/libs")
                 .Include("~/Scripts/jquery-{version}.js")
                 .Include("~/Scripts/bootstrap.js")
@@ -18,6 +20,13 @@ namespace Squirrel
                 .Include("~/Scripts/q.js")
                 .Include("~/Scripts/breeze*")
                 .Include("~/Scripts/jquery.ba-tinypubsub.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/index")
+                .Include("~/Scripts/sr.js")
+                .Include("~/Scripts/models/reminder.js")
+                .Include("~/Scripts/dataaccess/reminderRepository.js")
+                .Include("~/Scripts/viewmodels/index.js")
+                .Include("~/Scripts/view/index.js"));
 
             bundles.Add(new StyleBundle("~/Content/css")
                 .Include("~/Content/base.css")
